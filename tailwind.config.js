037a9215075631e07ba95f5728a1f1c6223a8abe
omarhsouna <omar.hsouna@gmail.com>
2022-09-24 18:40:42 +0200

    add responsive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        pink: "#FF385C",
+      },
+      boxShadow: {
+        loginButton: "0 2px 4px rgba(0,0,0,0.18)",
+      },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
