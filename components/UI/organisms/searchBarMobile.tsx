c1a83dc838690abba68cbe59bc65b50f700f8a18
omarhsouna <omar.hsouna@gmail.com>
2022-09-27 01:11:27 +0200

    add form first section search bar mobile

--- components/UI/organisms/searchBarMobile.tsx
+++ components/UI/organisms/searchBarMobile.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { IconContext } from "react-icons";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaSearch } from "react-icons/fa";
 const styleCheckedItem =
   "relative after:absolute after:left-0 after:-bottom-1 after:w-full after:h-[2px] after:bg-black text-black";
 const SearchBarMobile = () => {
   const [displayMenu, setDisplayMenu] = useState(false);
   const [checkedTab, setCheckedTab] = useState("stays");
+  const [activeSection, setActiveSection] = useState("where");
   return (
     <>
       <div
@@ -86,26 +87,105 @@ const SearchBarMobile = () => {
           </div>
         </div>
         {/* header */}
+        {/* form */}
+        {/* body */}
         <div className="flex flex-col mx-5 space-y-3">
-          <section className="bg-white rounded-2xl border border-gray-200 py-5 cursor-pointer">
-            <div className="flex items-center justify-between px-4">
-              <span className="text-sm font-semibold text-gray-500">Where</span>
-              <span className="text-sm font-semibold">I'm flexible</span>
-            </div>
+          <section
+            className="bg-white rounded-2xl border border-gray-200 py-5 cursor-pointer px-4"
+            onClick={() => setActiveSection("where")}
+          >
+            {activeSection === "where" ? (
+              <div>
+                <div className="font-bold text-[22px] text-gray-900 mb-4">
+                  Where to ?
+                </div>
+                <div className="relative mb-4">
+                  <input
+                    placeholder="Search destinations"
+                    type="search"
+                    className="h-[60px] border border-gray-200 rounded-lg w-full indent-14 placeholder:text-gray-500 outline-none"
+                  />
+                  <span className="absolute left-7 top-[36%] mt-[2px]">
+                    <FaSearch />
+                  </span>
+                </div>
+                <div className="flex space-x-4 flex-nowrap overflow-auto scroll-horizontal scroll-smooth">
+                  <div className="w-[132px] h-40 flex flex-col p-1 flex-shrink-0">
+                    <button className="h-[124px] h-[124px] rounded-lg border-2 border-gray-900 mb-auto"></button>
+                    <span>I'm flexible</span>
+                  </div>
+                  <div className="w-[132px] h-40 flex flex-col p-1 flex-shrink-0">
+                    <button className="h-[124px] h-[124px] rounded-lg border-2 border-gray-900 mb-auto"></button>
+                    <span>I'm flexible</span>
+                  </div>
+                  <div className="w-[132px] h-40 flex flex-col p-1 flex-shrink-0">
+                    <button className="h-[124px] h-[124px] rounded-lg border-2 border-gray-900 mb-auto"></button>
+                    <span>I'm flexible</span>
+                  </div>
+                  <div className="w-[132px] h-40 flex flex-col p-1 flex-shrink-0">
+                    <button className="h-[124px] h-[124px] rounded-lg border-2 border-gray-900 mb-auto"></button>
+                    <span>I'm flexible</span>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="flex items-center justify-between ">
+                <span className="text-sm font-semibold text-gray-500">
+                  Where
+                </span>
+                <span className="text-sm font-semibold">I'm flexible</span>
+              </div>
+            )}
           </section>
-          <section className="bg-white rounded-2xl border border-gray-200 py-5 cursor-pointer">
-            <div className="flex items-center justify-between px-4">
-              <span className="text-sm font-semibold text-gray-500">Where</span>
-              <span className="text-sm font-semibold">I'm flexible</span>
-            </div>
+          <section
+            className="bg-white rounded-2xl border border-gray-200 py-5 cursor-pointer px-4"
+            onClick={() => setActiveSection("when")}
+          >
+            {activeSection === "when" ? (
+              <div className="font-bold text-[22px] text-gray-900">
+                When's your trip?
+              </div>
+            ) : (
+              <div className="flex items-center justify-between">
+                <span className="text-sm font-semibold text-gray-500">
+                  When
+                </span>
+                <span className="text-sm font-semibold">Add dates</span>
+              </div>
+            )}
           </section>
-          <section className="bg-white rounded-2xl border border-gray-200 py-5 cursor-pointer">
-            <div className="flex items-center justify-between px-4">
-              <span className="text-sm font-semibold text-gray-500">Where</span>
-              <span className="text-sm font-semibold">I'm flexible</span>
-            </div>
+          <section
+            className="bg-white rounded-2xl border border-gray-200 py-5 cursor-pointer px-4"
+            onClick={() => setActiveSection("who")}
+          >
+            {activeSection === "who" ? (
+              <div className="font-bold text-[22px] text-gray-900">
+                Who's coming?
+              </div>
+            ) : (
+              <div className="flex items-center justify-between">
+                <span className="text-sm font-semibold text-gray-500">Who</span>
+                <span className="text-sm font-semibold">Add guests</span>
+              </div>
+            )}
           </section>
         </div>
+        {/* body */}
+        {/* footer */}
+        <footer className="absolute left-0 bottom-0 bg-footerForm w-full h-16 border-t border-gray-200 py-5 px-6 flex items-center justify-between">
+          <button className="font-semibold underline text-gray-900">
+            Clear all
+          </button>
+          <button className="flex items-center bg-buttonSearch text-white rounded-lg py-[9px] px-6 font-semibold ">
+            <IconContext.Provider value={{ className: "h-4 w-4 mt-[2px]" }}>
+              <FaSearch />
+            </IconContext.Provider>
+            <span className="ml-3">Search</span>
+          </button>
+        </footer>
+        {/* footer */}
+
+        {/* form */}
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        buttonSearch:
+          "linear-gradient(to right, rgb(230, 30, 77) 0%, rgb(227, 28, 95) 50%, rgb(215, 4, 102) 100%)",
+      },
       transitionDuration: {
         2000: "2000ms",
       },
       colors: {
         pink: "#FF385C",
         searchBarMenu: "#f7f7f7",
+        footerForm: "#ffffff80",
       },
       boxShadow: {
         loginButton: "0 2px 4px rgba(0,0,0,0.18)",
